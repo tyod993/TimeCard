ee411259b2a2b7ffb940d66569b9a173b960f656
Trevor Montgomery <45812229+tyod993@users.noreply.github.com>
2019-11-13 17:04:28 -0800

    few bugs fixed

---
+++ app/src/main/java/com/audiokontroller/timecard/ui/mainmenu/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.audiokontroller.timecard.ui.mainmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.audiokontroller.timecard.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
